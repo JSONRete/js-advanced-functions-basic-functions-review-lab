9e194ce78bd3718a577f9e8694cee29f0d32edf3
JSONRete <jsonrete@gmail.com>
2021-08-12 13:20:31 -0400

    commit tree ce97e6d684b62774f8b4bd59393472b9bd10aee8

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+function saturdayFun(test="roller-skate") {
+    return(`This Saturday, I want to ${test}!`);
+  }
+
+
+const mondayWork = function(r="go to the office") {
+return(`This Monday, I will ${r}.`);
+} 
+
+
+let wrapAdjective = function(style="*") {
+return function(zoo="special") {
+    return `You are ${style}${zoo}${style}!`
+    }
+
+  }
+
+let Calculator = {
+add: function(n1,n2) {
+    return n1 + n2;
+},
+
+subtract: function(n1,n2) {
+return n1 - n2;
+},
+
+multiply: function(n1,n2) {
+    return n1 * n2;
+},
+
+divide: function(n1,n2) {
+return n1 / n2;
+}
+}
+
+let actionApplyer = function(starting, array){
+    
+}
+
